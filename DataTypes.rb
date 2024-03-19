ee3c4f8acd920d613766074ecd1925b340f6d550
william arasirwa <arasirwawilliam22@gmail.com>
2024-03-19 12:50:00 +0300

    methods & data types complete

---
+++ DataTypes.rb
@@ -0,0 +1,12 @@
+# integer
+# float
+# string
+# float
+# boolean
+# symbol
+# Hash
+# arrays
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+puts (numbers.methods)
---
+++ Methods/Methods.rb
@@ -0,0 +1,21 @@
+# method to greet someone
+
+def greet_programmer
+    puts "Hello, I am a programmer" 
+end
+
+def greet(name) 
+    puts "Hello, #{name}"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}"
+end
+
+def add(num1, num2)
+    num1 + num2
+    sum = num1 + num2
+    return sum
+end
+
+
